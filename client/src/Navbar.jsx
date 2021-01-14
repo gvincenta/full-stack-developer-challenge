df61efa752b4b73486259c76b2b3b591ab529dec
gvincenta <gilbert11323@gmail.com>
2021-01-15 09:20:50 +1100

    fix navigation link

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap'
 export default function (){
     return (  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="books">Infoxchange</Navbar.Brand>
+    <Navbar.Brand href="/books">Infoxchange</Navbar.Brand>
     <Nav>
-      <Nav.Link href="books">Books</Nav.Link>
-      <Nav.Link href="authors">Authors</Nav.Link> 
+      <Nav.Link href="/books">Books</Nav.Link>
+      <Nav.Link href="/authors">Authors</Nav.Link> 
     </Nav> 
   </Navbar>)
 }
