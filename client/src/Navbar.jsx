c9c8e04b6ea84218f25b6e0e14f4ce6ac0846b17
gvincenta <gilbert11323@gmail.com>
2021-01-17 13:40:42 +1100

    navbar - highlight current section

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -4,8 +4,8 @@ export default function (){
     return (  <Navbar  style={{background:'#212121'}} variant='dark'>
     <Navbar.Brand href="/books">Infoxchange</Navbar.Brand>
     <Nav>
-      <Nav.Link href="/books">Books</Nav.Link>
-      <Nav.Link href="/authors">Authors</Nav.Link> 
+      <Nav.Link href="/books" active={window.location.pathname.search('/book') !== -1}>Books</Nav.Link>
+      <Nav.Link href="/authors" active={window.location.pathname.search('/author') !== -1}>Authors</Nav.Link> 
     </Nav> 
   </Navbar>)
 }
